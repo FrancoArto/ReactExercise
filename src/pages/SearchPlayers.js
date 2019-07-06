f4b6d5717055bbb861a67314ef4aca9083e0f2fd
Franco Arto <francoarto@gmail.com>
2019-07-06 20:23:24 -0300

    Created SearchPlayers page

---
+++ src/pages/SearchPlayers.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SearchPlayers = () => (
+  <div></div>
+)
+
+export default SearchPlayers;
---
+++ src/pages/index.js
@@ -0,0 +1,1 @@
+export { default as SearchPlayers } from './SearchPlayers';
