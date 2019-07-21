c8c036f768e17519c0b6f7917c2f28533b3e6451
Franco Arto <francoarto@gmail.com>
2019-07-21 00:13:55 -0300

    tested select and input methods

--- src/app-modules/core/components/CustomSelect/CustomSelect.test.js
+++ src/app-modules/core/components/CustomSelect/CustomSelect.test.js
@@ -1,11 +1,34 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { mount, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import CustomSelect from './CustomSelect';
 import positions from '../../../players/mocks/positions.json';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-it('renders without crashing', () => {
-  const rendered = shallow(<CustomSelect options={positions} />);
+describe('custom select', () => {
+  it('renders without crashing', () => {
+    const rendered = shallow(<CustomSelect options={positions} />);
+  });
+
+  it('handles value change', () => {
+    var inputValue = '';
+    const onValueChange = (value) => {
+      inputValue = value;
+      return value;
+    }
+    const wrapper = mount(<CustomSelect
+      onChange={onValueChange}
+      options={positions}
+      selectedValue={inputValue} />);
+
+    inputValue = 'Centre-Forward';
+    const selectInput = wrapper.find('CustomSelect').first();
+    selectInput.props().onChange(inputValue);
+    wrapper.setProps({
+      value: inputValue
+    });
+    wrapper.update();
+    expect(wrapper.props().value).toEqual(inputValue);
+  });
 });
--- src/app-modules/core/components/Input/Input.test.js
+++ src/app-modules/core/components/Input/Input.test.js
@@ -1,10 +1,32 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { mount, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Input from './Input';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-it('renders without crashing', () => {
-  const rendered = shallow(<Input />);
+describe('input component', () => {
+  it('renders without crashing', () => {
+    const rendered = shallow(<Input />);
+  });
+
+  it('handles value change', () => {
+    var inputValue = '';
+    const onValueChange = (value) => {
+      inputValue = value;
+      return value;
+    }
+    const wrapper = mount(<Input
+      onChange={onValueChange}
+      value={inputValue} />);
+
+    inputValue = 'Hola';
+    const textInput = wrapper.find('Input').first();
+    textInput.props().onChange(inputValue);
+    wrapper.setProps({
+      value: inputValue
+    });
+    wrapper.update();
+    expect(wrapper.props().value).toEqual(inputValue);
+  });
 });
