a54b0d4ec8ff3dc39bb42b55184bd0a5ba544113
Franco Arto <francoarto@gmail.com>
2019-09-06 10:21:08 -0300

    Fixed fields losing focus

--- src/app-modules/players/components/SearchForm/SearchForm.js
+++ src/app-modules/players/components/SearchForm/SearchForm.js
@@ -3,6 +3,10 @@ import { Input, CustomButton, CustomSelect } from '../../../core/components';
 import { Grid, makeStyles } from '@material-ui/core';
 import { Field, reduxForm } from 'redux-form';
 
+const NameInput = props => <Input fullWidth type="text" label="Player name" onChange={props.input.onChange} value={props.input.value} variant="outlined" />
+
+const AgeInput = props => <Input fullWidth type="number" InputProps={{ inputProps: { max: 40, min: 18 } }} label="Age" onChange={props.input.onChange} value={props.input.value} variant="outlined" />
+
 let SearchForm = (props) => {
 
   const { positions } = props
@@ -13,28 +17,22 @@ let SearchForm = (props) => {
     <form onSubmit={props.handleSubmit}>
       <Grid container className={classes.container} justify="center" spacing={2}>
         <Grid item xs={12} sm={3}>
-          <Field name="playerName" component={props =>
-            <Input fullWidth type="text" label="Player name" onChange={props.input.onChange} value={props.input.value} variant="outlined" />
-          }
+          <Field name="playerName" component={NameInput}
           />
         </Grid>
         <Grid item xs={12} sm={3}>
-          <Field name="playerPosition" component={props =>
-            <CustomSelect
-              fullWidth
-              id="positions-select"
-              label="Position"
-              labelWidth={60}
-              options={positions}
-              onValueChange={props.input.onChange}
-              selectedValue={props.input.value}
-              variant="outlined" />
-          } />
+          <Field name="playerPosition" component={props => <CustomSelect
+            fullWidth
+            id="positions-select"
+            label="Position"
+            labelWidth={60}
+            options={positions}
+            onValueChange={props.input.onChange}
+            selectedValue={props.input.value}
+            variant="outlined" />} />
         </Grid>
         <Grid item xs={12} sm={3}>
-          <Field name="playerAge" component={props =>
-            <Input fullWidth type="number" InputProps={{ inputProps: { max: 40, min: 18 } }} label="Age" onChange={props.input.onChange} value={props.input.value} variant="outlined" />
-          } />
+          <Field name="playerAge" component={AgeInput} />
         </Grid>
         <Grid item xs={12} sm={2}>
           <CustomButton fullWidth color="primary" type="submit" text="Search" variant="contained" />
